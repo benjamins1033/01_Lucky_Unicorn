0a06a38aef2e9389f0818398dde9a7d6e6822160
benjamins1033 <101072562+benjamins1033@users.noreply.github.com>
2023-02-23 10:28:33 +1300

    Add files via upload

--- 01_yes_no_v2.py
+++ 01_yes_no_v2.py
@@ -3,16 +3,12 @@ while show_instructions.lower() != "xxx":
 
     show_instructions = input("Have you played this game"
                               "before? ").lower()
-    if show_instructions == "yes":
+    if show_instructions == "yes" or show_instructions == "y":
+        show_instructions = "yes"
         print("program continues")
 
-    elif show_instructions == "y":
-        print("program continues")
-
-    elif show_instructions == "no":
+    elif show_instructions == "no" or show_instructions == "n":
         print("display instructions")
 
-    elif show_instructions == "n":
-        print("display instructions")
     else:
         print("Please answer yes / no")
