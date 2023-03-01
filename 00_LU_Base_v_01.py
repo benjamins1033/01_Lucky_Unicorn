b1edf937324256858aa09515e600a06fae9b76e9
benjamins1033 <101072562+benjamins1033@users.noreply.github.com>
2023-03-02 11:02:16 +1300

    Add files via upload

--- 00_LU_Base_v_01.py
+++ 00_LU_Base_v_01.py
@@ -51,9 +51,56 @@ played_before = yes_no("Have you played the "
 if played_before == "no":
     instructions()
 
-print("Program continues")
+print()
 
 how_much = num_check("How much would you "
                      "like to play with? ", 0, 10)
 
-print("You will be spending ${}".format(how_much))
+
+# set balance for testing purposes
+balance = how much
+
+rounds_played = 0
+
+play_again = input("Press <Enter> to play...").lower()
+while play_again == "":
+    # increase # of rounds played
+    rounds_played += 1
+
+    # Print round number
+    print("*** Round #{} ***".format(rounds_played))
+
+    chosen_num = random.randint(1, 100)
+
+    # Adjust balance
+    # if the random # is between 1 and 5,
+    # user gets a unicorn (add $4 to balance)
+    if 1 <= chosen_num <= 5:
+        chosen = "unicorn"
+        balance += 4
+
+    # if the random # is between 6 and 36
+    # user gets a donkey (subtract $1 from balance)
+    elif 6 <= chosen_num <= 36:
+        chosen = "donkey"
+        balance -= 1
+    else:
+        if chosen_num % 2 == 0:
+            chosen = "horse"
+        else:
+            chosen = "zebra"
+        balance -= 0.5
+
+    if balance < 1:
+        play_again = "xxx"
+        print("Sorry you have run out of money")
+    else:
+        play_again = input("Press Enter to play again "
+                           "or 'xxx' to quit")
+
+    print()
+    # print("You got {}. Your balance is "
+    #       "${:.2f}".format(chosen, balance))
+    print(f"You got {chosen}.  Your balance is {balance:.2f}")
+
+    print()
